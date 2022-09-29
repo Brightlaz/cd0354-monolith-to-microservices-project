f7672f35c9dc7ab03fe7493df283e4707d52a2b0
Bright Lazarus <lazbright1@gmail.com>
2022-09-29 16:19:21 +0100

    Structuring the code

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
+export POSTGRES_USERNAME=bright
+export POSTGRES_PASSWORD=123bright%
 export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_DB=postgres #  export  your env variable from my terminal
+export AWS_BUCKET=mybucket-123-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
