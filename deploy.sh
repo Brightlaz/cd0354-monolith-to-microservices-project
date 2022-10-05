9b7a461cbe5126c2fd9e663d6e72de1b95bc0deb
Bright Lazarus <lazbright1@gmail.com>
2022-10-05 02:40:40 +0100

    New version of frontend

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+kubectl delete deploy reverseproxy
+kubectl delete deploy backend-user
+kubectl delete deploy backend-feed
+kubectl delete deploy frontend
+
+kubectl apply -f feed-deploy.yml
+kubectl apply -f user-deploy.yml
+kubectl apply -f reverseproxy-deploy.yml
+kubectl apply -f frontend-deploy.yml
--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://aefe4359a8498451c87c385046685ace-565066032.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
